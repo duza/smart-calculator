9312e45604332d897db9462881b4c77834baa40e
duza <duza@tut.by>
2018-10-31 01:51:08 +0300

    Add a solution from task analysis

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.num = initialValue + '';
   }
 
   add(number) {
-    // your implementation
+    this.num = `${this.num} + ${number}`;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.num = `${this.num} - ${number}`;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.num = `${this.num} * ${number}`;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.num = `${this.num} / ${number}`;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.num = `${this.num} ** ${number}`;
+    return this;
+  }
+
+  toString() {
+    return eval(`${this.num}`);
   }
 }
 
